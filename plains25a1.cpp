7952752ddc5964d0e0e5a4f6de3ed1ef74c522b1
tomereliel1 <tomereliel@Tomer-pc>
2024-12-15 16:32:29 +0200

    adding given files

---
+++ plains25a1.cpp
@@ -0,0 +1,60 @@
+// You can edit anything you want in this file.
+// However you need to implement all public Plains function, as provided below as a template
+
+#include "plains25a1.h"
+
+
+Plains::Plains()
+{
+    
+}
+
+Plains::~Plains()
+{
+    
+}
+
+StatusType Plains::add_herd(int herdId)
+{
+    return StatusType::FAILURE;
+}
+
+StatusType Plains::remove_herd(int herdId)
+{
+    return StatusType::FAILURE;
+}
+
+StatusType Plains::add_horse(int horseId, int speed)
+{
+    return StatusType::FAILURE;
+}
+
+StatusType Plains::join_herd(int horseId, int herdId)
+{
+    return StatusType::FAILURE;
+}
+
+StatusType Plains::follow(int horseId, int horseToFollowId)
+{
+    return StatusType::FAILURE;
+}
+
+StatusType Plains::leave_herd(int horseId)
+{
+    return StatusType::FAILURE;
+}
+
+output_t<int> Plains::get_speed(int horseId)
+{
+    return 0;
+}
+
+output_t<bool> Plains::leads(int horseId, int otherHorseId)
+{
+    return false;
+}
+
+output_t<bool> Plains::can_run_together(int herdId)
+{
+    return false;
+}
---
+++ plains25a1.h
@@ -0,0 +1,50 @@
+// 
+// 234218 Data Structures 1.
+// Semester: 2025A (Winter).
+// Wet Exercise #1.
+// 
+// The following header file contains all methods we expect you to implement.
+// You MAY add private methods and fields of your own.
+// DO NOT erase or modify the signatures of the public methods.
+// DO NOT modify the preprocessors in this file.
+// DO NOT use the preprocessors in your other code files.
+// 
+
+#ifndef PLAINS25WINTER_WET1_H_
+#define PLAINS25WINTER_WET1_H_
+
+#include "wet1util.h"
+
+class Plains {
+private:
+    //
+    // Here you may add anything you want
+    //
+    
+public:
+    // <DO-NOT-MODIFY> {
+    Plains();
+
+    ~Plains();
+
+    StatusType add_herd(int herdId);
+
+    StatusType remove_herd(int herdId);
+
+    StatusType add_horse(int horseId, int speed);
+
+    StatusType join_herd(int horseId, int herdId);
+
+    StatusType follow(int horseId, int horseToFollowId);
+
+    StatusType leave_herd(int horseId);
+
+    output_t<int> get_speed(int horseId);
+
+    output_t<bool> leads(int horseId, int otherHorseId);
+
+    output_t<bool> can_run_together(int herdId);
+    // } </DO-NOT-MODIFY>
+};
+
+#endif // PLAINS25WINTER_WET1_H_
