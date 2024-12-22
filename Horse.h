f90a820b7633bfa5cd00512cbf3f8ed97d4df0ee
Omer-Shapira <omer.shapira@campus.technion.ac.il>
2024-12-22 19:04:06 +0200

    changing horse

--- Horse.h
+++ Horse.h
@@ -4,9 +4,15 @@ class Horse {
 private:
     int m_id;
     int m_speed;
+<<<<<<< HEAD
     shared_ptr<Herd> m_herd; //herd
     shared_ptr<Horse> m_follow; //leader's fake horse
     shared_ptr<Horse> m_followBy;//self fake horse
+=======
+    Herd* m_herd;
+    Horse* m_follow;
+    Horse* m_followBY;
+>>>>>>> 71e265b (changing horse)
     int m_followersNum;
 
 public:
